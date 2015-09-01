6838dbd457c2594ea9dc503a8875b90c5034727f
Hoang Nam <namhoag.td09@gmail.com>
2015-09-01 19:49:02 +0700

    Initial commit

---
+++ mailer2/public/js/app.js
@@ -0,0 +1,41 @@
+var  app = angular.module('mailer', []);
+
+app.controller('MailController', ['$scope', '$http', function($scope, $http) {
+	$scope.mail = {'from': 'namho102@hotmail.com'};
+
+	$scope.sendMail = function() {
+		// console.log($scope.mail);
+
+        $http.post('/send', $scope.mail)
+            .then(function(response) {
+                if(response.data == 'success') {
+                    console.log('mail is sent successfully');
+                    $scope.mail = null;
+                }
+            }, function(err) {
+                console.log('error: ' + err);
+            });
+	
+	}
+}]);
+
+// app.service('mailService', ['$http', '$q', function($http, $q) {
+//     var sendEmail = function(mail) {
+//         var d = $q.defer();
+//         $http({
+//             method: 'POST',
+//             data: mail,
+//             url: '/send'
+//         }).success(function (data) {
+//             d.resolve(data);
+//         })
+//         .error(function (data) {
+//             d.reject(data);
+//         });
+//         return d.promise;
+//     };
+
+//     return {
+//         sendEmail : sendEmail
+//     }
+// }]);
