0d2a17423cd0835e8c5f63f99948a2365469faf2
Hoang Nam <namhoang.td09@gmail.com>
2015-09-01 20:07:34 +0700

    Update app.js

--- mailer2/public/js/app.js
+++ mailer2/public/js/app.js
@@ -4,7 +4,7 @@ app.controller('MailController', ['$scope', '$http', function($scope, $http) {
 	$scope.mail = {'from': 'YOUR EMAIL'};
 
 	$scope.sendMail = function() {
-		// console.log($scope.mail);
+	// console.log($scope.mail);
 
         $http.post('/send', $scope.mail)
             .then(function(response) {
